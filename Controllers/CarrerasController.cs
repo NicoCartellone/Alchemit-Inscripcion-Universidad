81738ce856a07434b5f93a60b3afa7c13cf101db
Constanza Guimaraez <contiguima@gmail.com>
2022-07-11 07:56:38 -0300

    Agregue el controlador Carrera

---
+++ Controllers/CarrerasController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Inscripcion_Universidad.Models;
+using Inscripcion_Universidad.Models.Dominio;
+
+namespace Inscripcion_Universidad.Controllers
+{
+    public class CarrerasController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Carreras
+        public ActionResult Index()
+        {
+            return View(db.Carrera.ToList());
+        }
+
+        // GET: Carreras/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Carrera carrera = db.Carrera.Find(id);
+            if (carrera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carrera);
+        }
+
+        // GET: Carreras/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Carreras/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,NombreCarrera")] Carrera carrera)
+        {
+            if (ModelState.IsValid)
+            {
+                carrera.Id = Guid.NewGuid();
+                db.Carrera.Add(carrera);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(carrera);
+        }
+
+        // GET: Carreras/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Carrera carrera = db.Carrera.Find(id);
+            if (carrera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carrera);
+        }
+
+        // POST: Carreras/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,NombreCarrera")] Carrera carrera)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(carrera).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(carrera);
+        }
+       
+    }
+}
